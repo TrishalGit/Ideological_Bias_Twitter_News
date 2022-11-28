843d8f31ab1da59ac30d98231956fcbbae2acd77
TrishalGayam <96170761+TrishalGit@users.noreply.github.com>
2022-11-27 18:18:36 -0700

    File path fixes

--- data_parser/FastTextDatasetGenerator.py
+++ data_parser/FastTextDatasetGenerator.py
@@ -3,12 +3,16 @@ import csv
 
 directory = os.getcwd()
 
-train_filenames = ['Left_Train', 'Right_Train', 'Neutral_Train']
-test_filenames = ['Left_Test', 'Right_Test', 'Neutral_Test']
+#train_filenames = ['Left_Train', 'Right_Train', 'Neutral_Train']
+#test_filenames = ['Left_Test', 'Right_Test', 'Neutral_Test']
+
+# Comment the above lines and uncomment the below to run code on Harvard Dataset
+train_filenames = ['Left_Harvard_Train', 'Right_Harvard_Train', 'Neutral_Harvard_Train']
+test_filenames = ['Left_Harvard_Test', 'Right_Harvard_Test', 'Neutral_Harvard_Test']
 
 
 def save_file(data, filename):
-    file = open(directory + '/../data/pre_processed/' + filename, 'w')
+    file = open(directory + '/../data/fasttext/' + filename, 'w')
     file.writelines(data)
     file.close()
 
